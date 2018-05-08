34cd02dae6ee1df4a1ea3d59405a73bbdd1b149d
agungTuanany <agung.tuanany@gmail.com>
2018-05-09 00:26:19 +0700

    Server Structure Refactor and add Mongodb with mongoose

--- server/index.js
+++ server/index.js
@@ -1,31 +1,14 @@
+require('./services/passport');
+
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('./config/keys');
 
+mongoose.connect(keys.mongooseURI);
 const app = express();
+require('./routes/authRoutes')(app);
 const PORT = process.env.PORT || 5000;
 
-passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    }, (accessToken, refreshToken, profile, done) => {
-        console.log('access token',accessToken);
-        console.log('refresh token', refreshToken);
-        console.log('profile:', profile);
-    })
-);
-
-app.get(
-    '/auth/google',
-    passport.authenticate('google', {
-        scope: ['profile', 'email']
-    })
-);
-
-app.get('auth/google/callback', passport.authenticate('google'));
 
 app.listen(PORT, () => {
     console.log(`Started up at port ${PORT}`);
