1eebc3891673232c90becd2fd6e9999498d98cbe
agungTuanay <agung.tuanany@gmail.com>
2018-05-10 17:02:49 +0700

    Refactoring App component to class base component

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -6,9 +6,17 @@
 @param Route : is a object of react component it's use to setup a rule
  between certain route that user might visit inside our application and
  set of the component so that will be actualy visible on the screen
-*/
 
-import React from 'react';
+ @param componentDidMount:
+In future version of react the componentWillMount() method might be getting called multiple time automatically.
+By convention componentDidMount() look upon as prefer location
+ to make any tipe initial AJAX request
+
+Essentially no big different with componentDidMount() and componentWillMout() from speed and request
+
+ */
+
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 //dumy commponets
@@ -17,19 +25,28 @@ const Dashboard = () => <h2> Dashboard </h2>
 const SurveyNew = () => <h2> SurveyNew </h2>
 const Landing = () => <h2> Landing </h2>
 
-const App = () => {
-    return (
-        <div className="container">
-            <BrowserRouter>
-                <div>
-                    <Header />
-                    <Route exact path="/" component={Landing} />
-                    <Route exact path="/surveys" component={Dashboard} />
-                    <Route path="/surveys/new" component={SurveyNew} />
-                </div>
-            </BrowserRouter>
-        </div>
-    );
-};
+
+class App extends Component {
+    // add on lifecycle method for fetch the current user
+    componentDidMount() {
+
+    }
+
+
+    render() {
+        return (
+            <div className="container">
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route exact path="/" component={Landing} />
+                        <Route exact path="/surveys" component={Dashboard} />
+                        <Route path="/surveys/new" component={SurveyNew} />
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
+}
 
 export default App;
