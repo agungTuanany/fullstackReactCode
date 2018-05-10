396be9afe04af4b553d20c877cb1f68fbf42c3d7
agungTuanany <agung.tuanany@gmail.com>
2018-05-10 08:27:13 +0700

    client-React-Setup with Redux-React-Router and refactor passport.js

---
+++ server/client/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            Hi There this is Component!
+        </div>
+    );
+};
+
+export default App;
--- server/services/passport.js
+++ server/services/passport.js
@@ -22,16 +22,20 @@ passport.use(
             callbackURL: '/auth/google/callback',
             proxy: true
         },
-        (accessToken, refreshToken, profile, done) => {
-            User.findOne({googleId: profile.id}).then((existingUser) => {
-                if (existingUser) {
-                    done(null, existingUser);
-                } else {
-                    new User({googleId: profile.id})
-                        .save()
-                        .then(user => done(null, user));
+        async (accessToken, refreshToken, profile, done) => {
+
+            try {
+                const exisistingUser = await User.findOne({googleId: profile.d});
+
+                if (exisistingUser) {
+                    return done(null, exisistingUser)
                 }
-            });
+
+                const user = await new User({googleId: profile.id}).save();
+                done(null, user);
+            } catch (e) {
+                console.log(e);
+            }
 
         }
     )
