747d72526ef5ef172c1f115b7f37a863f3633eb8
agungTuanay <agung.tuanany@gmail.com>
2018-05-10 12:50:28 +0700

    Redux Setup authReducers

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -16,9 +16,10 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
 
 // make a redux store
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
     <Provider store={store}><App /></Provider>,
---
+++ server/client/src/reducers/authReducer.js
@@ -0,0 +1,8 @@
+// redux reducer
+
+export default function(state = {}, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
