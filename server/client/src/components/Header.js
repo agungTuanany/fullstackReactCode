e093f221bc37d63cf5ccec8d32814a94a04f8cea
agungTuanay <agung.tuanany@gmail.com>
2018-05-10 18:59:14 +0700

    Accesing State in the Header 02.Using auth property to decide what content to show inside the header

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -5,8 +5,18 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return;
+            case false:
+                return <li><a href="/auth/google">Login With Google</a></li>;
+            default:
+                return<li><a>Logout</a></li>
+        }
+    }
+
     render() {
-        console.log(this.props);
         return (
             <nav>
                 <div className="nav-wrapper">
@@ -14,9 +24,7 @@ class Header extends Component {
                     Emaily
                     </a>
                     <ul className="right">
-                        <li>
-                            <a>Login With Google</a>
-                        </li>
+                        {this.renderContent()}
                     </ul>
                 </div>
             </nav>
