89d627ba69a230b47502e43e3d460b7c1508e238
agungTuanay <agung.tuanany@gmail.com>
2018-05-11 10:51:07 +0700

    Adding action types to update user-model from authReducer to send back response User pays credits

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -6,13 +6,17 @@ basic Redux Thunk:
      a Actions and put it in Dispacth-Function back to Redux-Store.
     So redux-thunk is allowing us direct access to Dispacth-function.
 
-Dispacth-function:
+@params dispatch({}) / Dispacth-function:
     Dispacth sends the action to all the different reducers in the store,
      causing them to instantly recalculate the app state.
 
     Dispacth-function is belong to Redux-store. If we call Dispacth-function
      with an Action, the Actions will automatically forward it on to
      different reducer inside the application.
+
+Why using the same dispatch({}) can we DRY it?
+    The request that user send is different and that is kind of hard to generailze between of two
+    The reason is if fetchUser or handleToken might want to do some other stuff
 */
 
 // this index.js for action creator React-Components
@@ -22,11 +26,22 @@ import { FETCH_USER } from './types';
 export const fetchUser =  () => async dispacth => {
     try {
         const res =await axios.get('/api/current_user');
+
         dispacth({ type: FETCH_USER, payload: res.data });
     } catch (e){
         console.log(e);
     }
 };
+
+export const handleToken = (token) => async dispacth => {
+    try {
+        const res = await axios.post('/api/stripe, token');
+
+        dispacth({ type: FETCH_USER, payload: res.data });
+    } catch (e) {
+        console.log(e);
+    }
+}
 
 // asyc-await V.2
 // export const fetchUser =  () => async dispacth =>
