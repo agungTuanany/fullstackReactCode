d0ea6545069a0c59442d4d2d5aa03d6ced864798
agungTuanay <agung.tuanany@gmail.com>
2018-05-11 20:43:50 +0700

    Requiring Authentication for check login

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -3,18 +3,24 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
     app.post('/api/stripe', async (req, res) => {
-        const charge = await stripe.charges.create({
-            amount: 500,
-            currency: 'usd',
-            description: '$5 for 5 credit',
-            source: req.body.id
-        });
-        //update user Model
-        // the user in 'req.user' and 'const user' is represent the same user in separate object in memory
-        // whenever we save the user model by convention we make reference to the copy of the model / copy of the user we just get back from Document(database)
-        req.user.credits += 5;
-        const user = await req.user.save();
+        if (!req.user) {
+            return res.status(401).send({ error: "You must log in!" });
+        }
 
-        res.send(user);
+        try {
+            const charge = await stripe.charges.create({
+                amount: 500,
+                currency: 'usd',
+                description: '$5 for 5 credit',
+                source: req.body.id
+            });
+
+            req.user.credits += 5;
+            const user = await req.user.save();
+
+            res.send(user);
+        } catch (e) {
+            console.log(e);
+        }
     });
 };
