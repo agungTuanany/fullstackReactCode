8adf41c1122f60605342f76ba71dd476159bdb68
agungTuanay <agung.tuanany@gmail.com>
2018-05-12 00:03:10 +0700

    Define Relationship Fields and completed survey schema

--- server/models/Survey.js
+++ server/models/Survey.js
@@ -8,7 +8,12 @@ const surveySchema = new Schema({
     subject: String,
     recipients:[RecipientSchema],
     yes: { type: Number, default: 0 },
-    no: { type: Number, default: 0 }
+    no: { type: Number, default: 0 },
+    // define which survey belong to which user
+    // by convention using '_' of '_user' is telling this is suppose to be a relationship fields or reference field
+    _user:{ type: Schema.Types.ObjectId, ref: 'user' },
+    dateSent: Date,
+    lastResponded: Date
 });
 
 mongoose.model('surveys', surveySchema);
