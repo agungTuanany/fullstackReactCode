a82f4a8e65486a2215d17774014c15ce11a85f79
agungTuanay <agung.tuanany@gmail.com>
2018-05-12 00:56:04 +0700

    Survey Creation Route Handler 03.Create Instance Surveys

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,6 +1,27 @@
+/**
+@param req.body: is to get access to all different properties
+@param const{}: is to access all the properties with desctructuring
+@param const survey: lower case 'survey' indicate to instance of a survey
+
+*/
+
+const mongoose = require('mongoose');
+
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 
+const Survey = mongoose.model('surveys');
+
 module.exports = app => {
-    app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {});
+    app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+        const { title, subject, body, recipients } = req.body;
+
+        // using surveys mongoose model to create a new instance of survey, represent the survey to be send out to bunch of user inside of mongo database
+        const survey = new Survey({
+            title,
+            subject,
+            body
+        });
+
+    });
 };
