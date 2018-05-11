578f6c23b7e28ebbdfbd2f4239c5f652593494a6
agungTuanay <agung.tuanany@gmail.com>
2018-05-12 00:13:35 +0700

    Survey Creation Route Handler 01.Add surveyRoute.js

--- server/models/Survey.js
+++ server/models/Survey.js
@@ -9,8 +9,6 @@ const surveySchema = new Schema({
     recipients:[RecipientSchema],
     yes: { type: Number, default: 0 },
     no: { type: Number, default: 0 },
-    // define which survey belong to which user
-    // by convention using '_' of '_user' is telling this is suppose to be a relationship fields or reference field
     _user:{ type: Schema.Types.ObjectId, ref: 'user' },
     dateSent: Date,
     lastResponded: Date
