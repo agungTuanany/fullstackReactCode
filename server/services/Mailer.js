1701bc7507c4473d5b4f504664dc53eed84adc79
agungTuanay <agung.tuanany@gmail.com>
2018-05-12 03:24:16 +0700

    Creating Mailers 02.Mailer Constructor

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -1,8 +1,20 @@
+/**
+@param this,formatAdresses(): this keyword belong to helper function same as
+    sendgrid.mail().formatAddresses();
+*/
 const sendgrid = require('sendgrid');
 const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
 class Mailer extends helper.Mail {
+    constructor({ subject, recipients }, content) {
+        super();//es2015 classes
+
+        this.from_email = new helper.Email('no-reply@emaily.com');
+        this.subject = subject;
+        this.body = new helper.Content('text/html', content);
+        this.recipients = this.formatAddresses(recipients);
+    }
 
 };
 
