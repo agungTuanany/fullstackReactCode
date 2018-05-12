4b0bf0376c88fc5ee9fce162288ca31e1ca98cf9
agungTuanay <agung.tuanany@gmail.com>
2018-05-12 21:31:02 +0700

    Create Custom Field Component

---
+++ server/client/src/components/surveys/SurveyField.js
@@ -0,0 +1,10 @@
+// SurveyField contains logic to render a single label and text input
+import React from 'react';
+
+export default ({ input, label, meta: { error, touched } }) => {
+    return (
+        <div>
+            <input />
+        </div>
+    );
+};
