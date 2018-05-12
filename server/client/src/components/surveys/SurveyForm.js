5579e070222caa007e5137945355f7f14f1f8328
agungTuanay <agung.tuanany@gmail.com>
2018-05-12 23:44:46 +0700

    Creating an array for models out forn Fields

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -1,22 +1,37 @@
 /**
 @ param { reduxForm }: is a helper to allow communicate with redux store
  take care calling action-creator, pulling data from redux-store and providing another component.
-
- Clearly reduxForm is a connect helper library.
+Clearly reduxForm is a connect helper library.
  */
-// SurveyForm shows a form for a user to add input
+
+ // SurveyForm shows a form for a user to add input
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField';
 
+const FIELDS = [
+    { label: 'Survey Title', name: 'title'},
+    { label: 'Subject Line', name: 'subject'},
+    { label: 'Email Body', name: 'body'},
+    { label: 'Recipient List', name: 'emails'}
+];
+
 class SurveyForm extends Component {
     renderFields() {
-        return (
-            <div>
-                <Field type="text" name="title" component={SurveyField} />
-            </div>
-        )
+        return _.map(FIELDS, ({ label, name }) => {
+            return (
+                <Field
+                    key={name}
+                    component={SurveyField}
+                    type="type"
+                    label={label}
+                    name={name}
+                />
+            );
+        });
     }
+
     render() {
         return (
             <div>
