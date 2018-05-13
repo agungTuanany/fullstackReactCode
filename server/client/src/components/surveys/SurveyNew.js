8dfffc734e7b0cd93d3c617120a2453d8c0a9b6b
agungTuanay <agung.tuanany@gmail.com>
2018-05-13 12:33:05 +0700

    Create SurveyFormReview & Component-Level-State at SurveyNew for called to SurveyForm with a callback

--- server/client/src/components/surveys/SurveyNew.js
+++ server/client/src/components/surveys/SurveyNew.js
@@ -1,12 +1,37 @@
+/**
+@param state = {fromReview: false} is equivalent with classic way
+    constructor(props) {
+        super(props);
+        this.state = {new: false};
+    }
+
+for creating component-level-state, with babel plugin that allow us
+ initialize the state with slightly different passion.
+
+@param onSurveySubmit: is a callback, was called if SurveyForm is successfully submitted on
+*/
 // SurveyNew shows SurveyForm and SurveyFormReview
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
+    state = { showFormReview: false };
+
+    renderContent() {
+        if (this.state.showFormReview) {
+            return <SurveyFormReview />
+        }
+
+        return <SurveyForm
+            onSurveySubmit={() => this.setState({ showFormReview: true })}
+        />
+    }
+
     render() {
         return (
             <div>
-                <SurveyForm />
+                {this.renderContent()}
             </div>
         );
     }
