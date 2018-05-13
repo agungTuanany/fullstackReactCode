8928e522d67c8a8081e26054c3190c7d38485f26
agungTuanay <agung.tuanany@gmail.com>
2018-05-14 06:45:27 +0700

    Condence All different lodash helper with Lodash Chain helper

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -28,20 +28,20 @@ module.exports = app => {
     });
 
     app.post('/api/surveys/webhooks', (req, res) => {
-        const events = _.map(req.body, ({email, url}) => {
-            const pathname = new URL(url).pathname
-            const p = new Path('/api/surveys/:surveyId/:choice');
-            const match = p.test(pathname);
-            if (match) {
+        const p = new Path('/api/surveys/:surveyId/:choice');
 
-                return {email, surveyId: match.surveyId, choice: match.Id};
-            }
-        });
-        // remove the element that 'undefined'
-        const compactEvents = _.compact(events);
-        const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+        const events = _.chain(req.body)
+            .map(({ email, url }) => {
+                const match = p.test(new URL(url).pathname);
+                if (match) {
+                    return {email, surveyId: match.surveyId, choice: match.Id};
+                }
+            })
+            // remove the element that 'undefined'
+            .compact()
+            .uniqBy('email', 'surveyId')
 
-        console.log(uniqueEvents);
+        console.log(events);
 
         res.send({});
     });
